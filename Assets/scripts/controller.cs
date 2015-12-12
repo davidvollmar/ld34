b7eb7a0cf3cfac8fe717d0fd0d47a2694b1e6d43
beurskensm <beurskensm@gmail.com>
2015-12-12 20:32:59 +0100

    changes

--- Assets/scripts/controller.cs
+++ Assets/scripts/controller.cs
@@ -5,19 +5,32 @@ using System;
 public class controller : MonoBehaviour {
 
     public GameObject gcamera;
+    private Rigidbody rb;
+    private bool hitLevel = false;
 
 	// Use this for initialization
 	void Start () {
-	
+        rb = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        var value = Input.GetAxis("Horizontal");
-        Debug.logger.Log("game", "horizontal" + value);
-        Vector3 trans = new Vector3(value * 0.2f * (float)Math.PI, 0, 0);
-        transform.Translate(trans);
-        transform.Rotate(0, 0, value * -5);
-        gcamera.transform.Translate(trans);
+        float value = 0f;
+            value = Input.GetAxis("Horizontal");
+        if (!hitLevel)
+            value *= .4f;
+        var force = new Vector3((value *.5f), 0, 0);
+        rb.AddForce(force, ForceMode.Impulse);
+        gcamera.transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - 10);
+    }
+    void OnCollisionEnter(Collision col)
+    {
+        if(col.collider.name == "level")
+            hitLevel = true;
+    }
+    void OnCollisionExit(Collision col)
+    {
+        if (col.collider.name == "level")
+            hitLevel = false;
     }
 }
