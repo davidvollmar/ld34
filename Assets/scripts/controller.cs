1d7f83b644fbb3f28903d2a45709207d2902c1eb
David Vollmar <david@vollmar.nl>
2015-12-12 18:24:36 +0100

    init

---
+++ Assets/scripts/controller.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class controller : MonoBehaviour {
+
+    public GameObject gcamera;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var value = Input.GetAxis("Horizontal");
+        Debug.logger.Log("game", "horizontal" + value);
+        Vector3 trans = new Vector3(value * 0.2f * (float)Math.PI, 0, 0);
+        transform.Translate(trans);
+        transform.Rotate(0, 0, value * -5);
+        gcamera.transform.Translate(trans);
+    }
+}
